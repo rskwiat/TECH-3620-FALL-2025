635850d5129562908fdeeeb7ce6af3dbc27eba90
Robert Skwiat <rob@robertskwiat.com>
2025-08-07 23:12:02 -0400

    init app

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import { config } from 'dotenv';
+import { serve } from '@hono/node-server';
+import { app } from './src/app.js';
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+
+config({
+  path: envFile
+});
+
+const { PORT } = process.env || 4000;
+
+console.log(
+  `Server is running on port http://localhost:${PORT}`
+);
+
+serve({
+  fetch: app.fetch,
+  port: PORT,
+});
